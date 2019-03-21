16069619806c5a7d073fd956b8d4a9ab44b1f304
齐晧 <qihao@raybattle.com>
2019-03-21 10:09:36 +0800

    update version 1.3.31

--- src/tools.js
+++ src/tools.js
@@ -100,6 +100,53 @@ export default {
         })
     },
 
+    /**
+     * 防抖函数
+     * @param method 事件触发的操作
+     * @param delay 多少毫秒内连续触发事件，不会执行
+     * @returns {Function}
+     */
+    $debounce(method, delay) {
+        let timer = null;
+        return function () {
+            let self = this;
+            let args = arguments;
+            timer && clearTimeout(timer);
+            timer = setTimeout(function () {
+                method.apply(self, args);
+            },delay);
+        }
+    },
+
+    /**
+     * 节流函数
+     * @param method 事件触发的操作
+     * @param mustRunDelay 间隔多少毫秒需要触发一次事件
+     */
+    $throttle(method, mustRunDelay) {
+        let timer;
+        let args = arguments;
+        let start;
+        return function loop() {
+            let self = this;
+            let now = Date.now();
+            if(!start){
+                start = now;
+            }
+            if(timer){
+                clearTimeout(timer);
+            }
+            if(now - start >= mustRunDelay){
+                method.apply(self, args);
+                start = now;
+            }else {
+                timer = setTimeout(function () {
+                    loop.apply(self, args);
+                }, 50);
+            }
+        }
+    },
+
     $setSession(key, value) {
         window.sessionStorage.setItem(key, JSON.stringify(value));
     },
